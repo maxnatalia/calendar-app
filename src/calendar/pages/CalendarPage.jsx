2764c3e2de792b212c5e5919d8fa6ff38dfea051
maxnatalia <Nataliamazur1988@gmail.com>
2024-08-06 08:44:51 +0200

    updated - implemented Navbar and Calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,7 +1,48 @@
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+import enUS from "date-fns/locale/en-US";
+
+import { Navbar } from "../";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "Cumpleanos del jefe",
+    notes: "Hay que comprar un pastel",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgColor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Natalia",
+    },
+  },
+];
+
 export const CalendarPage = () => {
   return (
     <>
-      <h1>CalendarPage</h1>
+      <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+      />
     </>
   );
 };
